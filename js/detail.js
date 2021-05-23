f64badfcb6fa809ce117820c8d0a9b1d5dd84204
juyeongsung <jooyongs1@gmail.com>
2021-05-23 18:13:56 +0900

    6시까지

---
+++ js/detail.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$('.share_btn').on('click',function(){
+    $('.url_btn').toggleClass('sharebtn_act');
+    $('.facebook_btn').toggleClass('sharebtn_act')
+    $(this).toggleClass('share_btn_color')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
